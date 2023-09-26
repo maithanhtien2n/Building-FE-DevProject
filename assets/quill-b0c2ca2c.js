e0d0cf745df6895cd76c40bdb80ae9cad3e8cc0f
Tiện Thanh <tienmt@imt-soft.com>
2023-09-26 17:27:10 +0700

    update code

--- assets/quill-b0c2ca2c.js
+++ assets/quill-b0c2ca2c.js
@@ -1,4 +1,4 @@
-import{i as yt,j as mt}from"./index-be50f945.js";function gt(ct,ht){for(var B=0;B<ht.length;B++){const _=ht[B];if(typeof _!="string"&&!Array.isArray(_)){for(const v in _)if(v!=="default"&&!(v in ct)){const P=Object.getOwnPropertyDescriptor(_,v);P&&Object.defineProperty(ct,v,P.get?P:{enumerable:!0,get:()=>_[v]})}}}return Object.freeze(Object.defineProperty(ct,Symbol.toStringTag,{value:"Module"}))}var dt={exports:{}};/*!
+import{i as yt,j as mt}from"./index-2819e244.js";function gt(ct,ht){for(var B=0;B<ht.length;B++){const _=ht[B];if(typeof _!="string"&&!Array.isArray(_)){for(const v in _)if(v!=="default"&&!(v in ct)){const P=Object.getOwnPropertyDescriptor(_,v);P&&Object.defineProperty(ct,v,P.get?P:{enumerable:!0,get:()=>_[v]})}}}return Object.freeze(Object.defineProperty(ct,Symbol.toStringTag,{value:"Module"}))}var dt={exports:{}};/*!
  * Quill Editor v1.3.7
  * https://quilljs.com/
  * Copyright (c) 2014, Jason Chen
